18fa61e0b45f8a321977459a19bef1152791c54a
GAGAN-ESWAR <gaganeswar.26@gmail.com>
2025-07-28 20:27:01 +0530

    Change BG
    change bakground color when mouse hovers over a
    grid square. call event listener over all applicable
    squares using foreach

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ for(let i=0;i<16*16;i++){
     square.classList.add("grid-square");
     container.appendChild(square);
 }
+
+function changeBg(event) {
+    event.target.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+let gSquares = document.querySelectorAll(".grid-square");
+gSquares.forEach(square => {
+    square.addEventListener("mouseover",changeBg);
+})
