6339db35c9c374b1d6981bbcf2e88136d5471387
GAGAN-ESWAR <gaganeswar.26@gmail.com>
2025-07-29 10:40:17 +0530

    make more dynamic
    user can give number of squares per side between 1
    and 100.

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 let container = document.querySelector("#container");
 
-for(let i=0;i<16*16;i++){
+function generateGrid(size) {
+  container.innerHTML= "";
+  for(let i=0;i<size*size;i++){
     let square = document.createElement("div");
     square.classList.add("grid-square");
     container.appendChild(square);
+    let squaresize = 640/size;
+    square.style.width = `${squaresize}px`;
+    square.style.height = `${squaresize}px`;
+  }
+
+  let gSquares = document.querySelectorAll(".grid-square");
+    gSquares.forEach(square => {
+    square.addEventListener("mouseover",changeBg);
+  })
 }
+
+generateGrid(16);
+
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", ()=>{
+  let num = prompt("Please gimme no of squares per side :), and please limit it to 100");
+  num =parseInt(num);
+  if(!isNaN(num) && num>0 && num<=100){
+    generateGrid(num);
+  }
+  else{
+    alert("Please enter between 1 and 100 :))");
+  }
+})
 
 function changeBg(event) {
     event.target.style.backgroundColor = getRandomHexColor();
@@ -18,8 +43,3 @@ function getRandomHexColor() {
   }
   return color;
 }
-
-let gSquares = document.querySelectorAll(".grid-square");
-gSquares.forEach(square => {
-    square.addEventListener("mouseover",changeBg);
-})
